8d915f2248e7cd899b2b8a8796142287207c6009
Rafael Shamilov <rafael_shamilov@mail.ru>
2016-05-23 13:12:50 +0300

    1

---
+++ price.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html lang="ru" class="lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html lang="ru" class="lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html lang="ru" class="lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html>
+<!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+	<title>Стоимость Услуг</title>
+	<meta name="description" content="" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="shortcut icon" href="favicon.png" />
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
+	<link rel="stylesheet" href="libs/font-awesome-4.2.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
+	<link rel="stylesheet" href="libs/owl-carousel/owl.carousel.css" />
+	<link rel="stylesheet" href="libs/countdown/jquery.countdown.css" />
+	<link rel="stylesheet" href="css/fonts.css" />
+	<link rel="stylesheet" href="css/main.css" />
+	<link rel="stylesheet" href="css/media.css" />
+        <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Anonymous+Pro' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <header class="top_header">
+        <div class="header_topline">
+            <div class="container">
+                <div class="col-md-12">
+                    <div class="row">
+                        <button class="auth_buttons hidden-md hidden-lg hidden-sm"><i class="fa fa-user" aria-hidden="true"></i></button>
+                        <div class="top_links">
+                            <a href="#">Вход</a> /
+                            <a href="#">Регистрация</a>
+                        </div>
+                        <div class="soc_buttons">
+                            <a href="#"><i class="fa fa-vk" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="col-md-12">
+                <div class="row">
+                    <a href="#" class="logo">RexDo</a>
+                <nav class="maian_mnu clearfix">
+                    <button class="main_mnu_button hidden-md hidden-lg"><i class="fa fa-bars" aria-hidden="true"></i></button>
+                    <ul>
+                        <li><a href="#">Главная</a></li>
+                        <li><a href="website_promotion.php">Продвижение сайта</a></li>
+                        <li><a href="#" style="background-color:#2787AF">Стоимость услуг</a></li>
+                        <li><a href="#">Контакты</a></li>
+                    </ul>
+                    <div class="top_contacts"><i class="fa fa-mobile" aria-hidden="true"></i> 8-800-800-80-80</div>
+                </nav>
+                    <h1>Центр продвижения сайтов в интернете</h1>
+                    <div class="slider_container">
+                        <div class="next_button"><i class="fa fa-angle-right" aria-hidden="true"></i></div>
+                        <div class="prev_button"><i class="fa fa-angle-left" aria-hidden="true"></i></div>
+                        <div class="carousel">
+                            <div class="slide_item"><img src="../images/11.jpg" alt></div>
+                            <div class="slide_item"><img src="../images/22.jpg" alt></div>
+                            <div class="slide_item"><img src="../images/33.jpg" alt></div>
+                            <div class="slide_item"><img src="../images/44.jpg" alt></div>
+                            <div class="slide_item"><img src="../images/55.jpg" alt></div>
+                            <div class="slide_item"><img src="../images/66.jpg" alt></div>
+                        </div>
+                    </div>
+                </div>                   
+            </div>
+        </div>
+    </header>
+    <section class="order">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="sell_descr">Закажи обратный звонок <i class="fa fa-arrow-right" aria-hidden="true"></i></div>
+                </div>
+                <div class="col-md-5">
+                    <div class="sell_right">
+                        <!--Вспомогательное окно на кнопку заказать разработку внизу-->
+                        <a href="#callback " class="button fancybox">Заказать звонок</a>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="main_price">
+        <div class="container">
+            <div class="col-md-12">
+                <div class="text1">
+                    <h2>Варианты продвижения</h2>
+                    <div class="text2">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <i class="fa fa-circle-thin" aria-hidden="true"></i>
+                                <h3>Продвижение сайта по позициям</h3>
+                                <p>Это классическое российское SEO, самый популярный вариант продвижения.
+                                Мы согласовываем с вами список ключевых слов и прописываем их в договоре.
+                                Отчетность - по наличию вашего сайта в ТОП-10 поисковых систем Яндекс и Google.
+                                Проверка позиций происходит ежедневно. Все просто и довольно понятно.</p>
+                            </div>
+                            <div class="col-md-3">
+                                <i class="fa fa-rub" aria-hidden="true"></i>
+                                <p><b>Стоимость</b></p>
+                                <p>от 15 000 руб. (регионы)<br>от 40 000 руб. (МСК и СПБ)<br><a>Подробнее</a></p>
+                                <i class="fa fa-clock-o" aria-hidden="true"></i>
+                                <p><b>Сроки</b><br>от 2 месяцев</p>
+                            </div>
+                            <div class="col-md-3">
+                                <a href="#callback" class="button1">Заказать</a>
+                                <a href="#callback" class="button1">Подробнее</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text2">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <i class="fa fa-circle-thin" aria-hidden="true"></i>
+                                <h3>Продвижение сайта по позициям</h3>
+                                <p>Это классическое российское SEO, самый популярный вариант продвижения.
+                                Мы согласовываем с вами список ключевых слов и прописываем их в договоре.
+                                Отчетность - по наличию вашего сайта в ТОП-10 поисковых систем Яндекс и Google.
+                                Проверка позиций происходит ежедневно. Все просто и довольно понятно.</p>
+                            </div>
+                            <div class="col-md-3">
+                                <i class="fa fa-rub" aria-hidden="true"></i>
+                                <p><b>Стоимость</b></p>
+                                <p>от 15 000 руб. (регионы)<br>от 40 000 руб. (МСК и СПБ)<br><a>Подробнее</a></p>
+                                <i class="fa fa-clock-o" aria-hidden="true"></i>
+                                <p><b>Сроки</b><br>от 2 месяцев</p>
+                            </div>
+                            <div class="col-md-3">
+                                <a href="#callback" class="button1">Заказать</a>
+                                <a href="#callback" class="button1">Подробнее</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text2">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <i class="fa fa-circle-thin" aria-hidden="true"></i>
+                                <h3>Продвижение сайта по позициям</h3>
+                                <p>Это классическое российское SEO, самый популярный вариант продвижения.
+                                Мы согласовываем с вами список ключевых слов и прописываем их в договоре.
+                                Отчетность - по наличию вашего сайта в ТОП-10 поисковых систем Яндекс и Google.
+                                Проверка позиций происходит ежедневно. Все просто и довольно понятно.</p>
+                            </div>
+                            <div class="col-md-3">
+                                <i class="fa fa-rub" aria-hidden="true"></i>
+                                <p><b>Стоимость</b></p>
+                                <p>от 15 000 руб. (регионы)<br>от 40 000 руб. (МСК и СПБ)<br><a>Подробнее</a></p>
+                                <i class="fa fa-clock-o" aria-hidden="true"></i>
+                                <p><b>Сроки</b><br>от 2 месяцев</p>
+                            </div>
+                            <div class="col-md-3">
+                                <a href="#callback" class="button1">Заказать</a>
+                                <a href="#callback" class="button1">Подробнее</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text2">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <i class="fa fa-circle-thin" aria-hidden="true"></i>
+                                <h3>Продвижение сайта по позициям</h3>
+                                <p>Это классическое российское SEO, самый популярный вариант продвижения.
+                                Мы согласовываем с вами список ключевых слов и прописываем их в договоре.
+                                Отчетность - по наличию вашего сайта в ТОП-10 поисковых систем Яндекс и Google.
+                                Проверка позиций происходит ежедневно. Все просто и довольно понятно.</p>
+                            </div>
+                            <div class="col-md-3">
+                                <i class="fa fa-rub" aria-hidden="true"></i>
+                                <p><b>Стоимость</b></p>
+                                <p>от 15 000 руб. (регионы)<br>от 40 000 руб. (МСК и СПБ)<br><a>Подробнее</a></p>
+                                <i class="fa fa-clock-o" aria-hidden="true"></i>
+                                <p><b>Сроки</b><br>от 2 месяцев</p>
+                            </div>
+                            <div class="col-md-3">
+                                <a href="#callback" class="button1">Заказать</a>
+                                <a href="#callback" class="button1">Подробнее</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Вспомогательное окно на кнопку заказать разработку-->
+        <div class="hidden">
+            <form id="callback" class="pop_form">
+                <h3>Заказать обратный звонок</h3>
+                <input type="text" name="name1" placeholder="Ваше имя" required>
+                <input type="text" name="phone" placeholder="Ваш телефон" required>
+                <button type="submit" class="button" name="application">Заказать</button>
+            </form>
+        </div>
+	<!--[if lt IE 9]>
+	<script src="libs/html5shiv/es5-shim.min.js"></script>
+	<script src="libs/html5shiv/html5shiv.min.js"></script>
+	<script src="libs/html5shiv/html5shiv-printshiv.min.js"></script>
+	<script src="libs/respond/respond.min.js"></script>
+	<![endif]-->
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
+	<script src="libs/jquery-mousewheel/jquery.mousewheel.min.js"></script>
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="libs/waypoints/waypoints-1.6.2.min.js"></script>
+	<script src="libs/scrollto/jquery.scrollTo.min.js"></script>
+	<script src="libs/owl-carousel/owl.carousel.min.js"></script>
+	<script src="libs/countdown/jquery.plugin.js"></script>
+	<script src="libs/countdown/jquery.countdown.min.js"></script>
+	<script src="libs/countdown/jquery.countdown-ru.js"></script>
+	<script src="libs/landing-nav/navigation.js"></script>
+	<script src="js/common.js"></script>
+	<!-- Yandex.Metrika counter --><!-- /Yandex.Metrika counter -->
+	<!-- Google Analytics counter --><!-- /Google Analytics counter -->
+</body>
+</html>
---
+++ website_promotion.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html lang="ru" class="lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html lang="ru" class="lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html lang="ru" class="lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html>
+<!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+	<title>Продвижение сайта</title>
+	<meta name="description" content="" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" /> <!--Подключение совместимости с edge -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" /> <!-- Нужно для создания адаптивного дизайна -->
+	<link rel="shortcut icon" href="favicon.png" /> <!-- фавиконка --> 
+	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" /> <!-- подключение бутстрап сетки -->
+	<link rel="stylesheet" href="libs/font-awesome-4.2.0/css/font-awesome.min.css" /> <!-- подключение иконок http://fontawesome.io/icons/ -->
+	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" /> <!-- менеджер попап кнопок fancybox (вспылающая форма,изображения и тд) -->
+	<link rel="stylesheet" href="libs/owl-carousel/owl.carousel.css" /> <!-- карсуль owl -->
+	<link rel="stylesheet" href="libs/countdown/jquery.countdown.css" /> <!-- таймер обратного отсчета -->
+	<link rel="stylesheet" href="css/fonts.css" /> <!-- шрифты по умолч для текста-->
+	<link rel="stylesheet" href="css/main.css" /> 
+	<link rel="stylesheet" href="css/media.css" />
+        <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Anonymous+Pro' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <header class="top_header">
+        <div class="header_topline">
+            <div class="container">
+                <div class="col-md-12">
+                    <div class="row">
+                        <button class="auth_buttons hidden-md hidden-lg hidden-sm"><i class="fa fa-user" aria-hidden="true"></i></button>
+                        <div class="top_links">
+                            <a href="#">Вход</a> /
+                            <a href="#">Регистрация</a>
+                        </div>
+                        <div class="soc_buttons">
+                            <a href="#"><i class="fa fa-vk" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+                            <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="col-md-12">
+                <div class="row">
+                    <a href="#" class="logo">RexDo</a>
+                <nav class="maian_mnu clearfix">
+                    <button class="main_mnu_button hidden-md hidden-lg"><i class="fa fa-bars" aria-hidden="true"></i></button>
+                    <ul>
+                        <li><a href="#">Главная</a></li>
+                        <li><a href="#" style="background-color:#2787AF">Продвижение сайта</a></li>
+                        <li><a href="price.php">Стоимость услуг</a></li>
+                        <li><a href="#">Контакты</a></li>
+                    </ul>
+                    <div class="top_contacts"><i class="fa fa-mobile" aria-hidden="true"></i> 8-800-800-80-80</div>
+                </nav>
+                    <h1>Центр продвижения сайтов в интернете</h1>
+                    <div class="slider_container">
+                        <div class="next_button"><i class="fa fa-angle-right" aria-hidden="true"></i></div>
+                        <div class="prev_button"><i class="fa fa-angle-left" aria-hidden="true"></i></div>
+                        <div class="carousel">
+                            <div class="slide_item"><img src="../images/11.jpg" alt></div>
+                            <div class="slide_item"><img src="../images/22.jpg" alt></div>
+                            <div class="slide_item"><img src="../images/33.jpg" alt></div>
+                            <div class="slide_item"><img src="../images/44.jpg" alt></div>
+                            <div class="slide_item"><img src="../images/55.jpg" alt></div>
+                            <div class="slide_item"><img src="../images/66.jpg" alt></div>
+                        </div>
+                    </div>
+                </div>                   
+            </div>
+        </div>
+    </header>
+    <section class="order">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="sell_descr">Закажи обратный звонок <i class="fa fa-arrow-right" aria-hidden="true"></i></div>
+                </div>
+                <div class="col-md-5">
+                    <div class="sell_right">
+                        <!--Вспомогательное окно на кнопку заказать разработку внизу-->
+                        <a href="#callback " class="button fancybox">Заказать звонок</a>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="main_content">
+        <div class="container">
+            <div class="col-md-8">
+                <div class="content_text">
+                    <div class="blog_item">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3><a href="#">Заголовок статьи</a></h3>
+                            </div>
+                            <div class="col-md-4">
+                                <img src="../images/11.jpg" alt="alt">
+                            </div>
+                            <div class="col-md-8">
+                                <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.</p>
+                                <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="blog_item">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3><a href="#">Заголовок статьи</a></h3>
+                            </div>
+                            <div class="col-md-4">
+                                <img src="../images/22.jpg" alt="alt">
+                            </div>
+                            <div class="col-md-8">
+                                <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.</p>
+                                <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="blog_item">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3><a href="#">Заголовок статьи</a></h3>
+                            </div>
+                            <div class="col-md-4">
+                                <img src="../images/33.jpg" alt="alt">
+                            </div>
+                            <div class="col-md-8">
+                                <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.</p>
+                                <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <aside class="right_aside">
+                    <ul>
+                        <li>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.</li>
+                        <li>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.</li>
+                        <li>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.</li>
+                        <li>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.</li>
+                        <li>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.</li>
+                        <li>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.</li>
+                        <li>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.</li>
+                        <li>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.</li>
+                        <li>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.</li>
+                        <li>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.</li>
+                    </ul>
+                
+                </aside>
+            </div>
+        </div>
+    </section>
+    <!--Вспомогательное окно на кнопку заказать разработку-->
+        <div class="hidden">
+            <form id="callback" class="pop_form">
+                <h3>Заказать обратный звонок</h3>
+                <input type="text" name="name1" placeholder="Ваше имя" required>
+                <input type="text" name="phone" placeholder="Ваш телефон" required>
+                <button type="submit" class="button" name="application">Заказать</button>
+            </form>
+        </div>
+	<!--[if lt IE 9]>
+	<script src="libs/html5shiv/es5-shim.min.js"></script>
+	<script src="libs/html5shiv/html5shiv.min.js"></script>
+	<script src="libs/html5shiv/html5shiv-printshiv.min.js"></script>
+	<script src="libs/respond/respond.min.js"></script>
+	<![endif]-->
+	<script src="libs/jquery/jquery-1.11.1.min.js"></script> <!-- подключение jquery -->
+	<script src="libs/jquery-mousewheel/jquery.mousewheel.min.js"></script>
+	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="libs/waypoints/waypoints-1.6.2.min.js"></script>
+	<script src="libs/scrollto/jquery.scrollTo.min.js"></script>
+	<script src="libs/owl-carousel/owl.carousel.min.js"></script>
+	<script src="libs/countdown/jquery.plugin.js"></script>
+	<script src="libs/countdown/jquery.countdown.min.js"></script>
+	<script src="libs/countdown/jquery.countdown-ru.js"></script>
+	<script src="libs/landing-nav/navigation.js"></script>
+	<script src="js/common.js"></script>
+	<!-- Yandex.Metrika counter --><!-- /Yandex.Metrika counter -->
+	<!-- Google Analytics counter --><!-- /Google Analytics counter -->
+</body>
+</html>
